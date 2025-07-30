38560362ce936cd30ce8b4fec520d20cb993916d
adil <58748338+faderzz@users.noreply.github.com>
2025-07-30 23:51:38 +0100

    config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['images.unsplash.com', 'upload.wikimedia.org', 'www.hubspot.com'],
     },
+    eslint: {
+        ignoreDuringBuilds: false, // Keep ESLint enabled but disable specific rules
+      },
 };
 
 export default nextConfig;
